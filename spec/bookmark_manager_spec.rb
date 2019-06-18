d507251e646d45ba0fa397f9e5e1e39b8d36e028
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-06-18 11:43:38 +0100

    Refactored index to improve scalability with the each method

--- spec/bookmark_manager_spec.rb
+++ spec/bookmark_manager_spec.rb
@@ -7,6 +7,8 @@ describe BookmarkManager do
     it 'displays all bookmarks' do
       expect(BookmarkManager.all).to be_instance_of(Array)
       expect(BookmarkManager.all.length).to eq 3
+      expect(BookmarkManager.all).to include("http://www.makersacademy.com")
+      expect(BookmarkManager.all).to include("http://www.destroyallsoftware.com")
     end
   end
 end
