89e4338489a6aeeeeb14fe72d100cc48ceaa6a2b
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-06-18 10:20:23 +0100

    Added missing semicolon in pg query

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -8,8 +8,7 @@ class BookmarkManager
 
   def all
     con = PG.connect( dbname: 'bookmark_manager' )
-    results = con.exec( "SELECT url FROM bookmarks" )
+    results = con.exec( 'SELECT url FROM bookmarks;' )
     results.map { |bookmarks| bookmarks['url'] }
   end
-
 end
